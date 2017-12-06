59d95283a01b5e1169d6b6feab0e9ddbf269bdd9
phillip shields <efe-in-the-mountains@users.noreply.github.com>
2017-12-07 05:46:29 +0800

    shorten extract length and begin styling
    extract section is too long, remove card classes on li

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     
     var searchBtn = document.getElementById("wikiBtn");
+  
 
     searchBtn.addEventListener("click", function (e) {
         e.preventDefault();
         searchApi();
-    })
+    });
     
     function searchApi() {
         var api = "https://en.wikipedia.org/w/api.php";
@@ -32,8 +33,8 @@ $(document).ready(function () {
                 'Api-User-Agent': 'Example/1.0'
               },
               success: function(data) {
-                // var pages = data.query.pages;
-                // displayPages(pages);
+                var pages = data.query.pages;
+                displayPages(pages);
                 console.dir(data);
               },
               error: function(err) {
@@ -42,6 +43,42 @@ $(document).ready(function () {
             });
           }
             
+          function displayPages(pages) {
+            var html = "";
+
+            $.each(pages, function(index, value) {
+              var url = value.fullurl;
+              var title = value.title;
+              var extract = value.extract;
+              var thumb = value.thumbnail.source;
+
+// change the way we add the info on the page
+
+              var topLi = "<li class='card-body d-flex flex-column align-items-center'>";
+              
+              var headerTop = "<div class='card-header px-2 d-flex flex-row justify-content-around'>";
+              var resultTitle =  "<h3 class='card-title'>" + title + "</h3>";
+              var img = "<img class='img-fluid' src='" + thumb + "'>";
+              var headerBottom = "</div>";
+              var resultInfo = "<p class='card-text'>" + extract + "</p>";
+              var resultUrl = "<a class='btn btn-link text-center' href='" + url + "'>" + url + "</a>";
+          
+              var bottomLi = "</li>";
+
+              html += topLi;
+              html += headerTop;
+              html += resultTitle;
+              html += img;
+              html += headerBottom;
+              html += resultInfo;
+              html += resultUrl;
+              html += bottomLi;
+
+            });
+
+            $("#resultsBox").html(html);
+          }
+
     function fetchSearchValue() {
         var s = $("#searchInput").val();
         if (!s) {
@@ -51,13 +88,5 @@ $(document).ready(function () {
           return s;
         }
     }    
-    
-    
-    
-    
-    
-    
-    
-    
     
 });
